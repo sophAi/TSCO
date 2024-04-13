7b48eaf3a837ec140282267408cf23bf05bfe27d
OASYS <clusterga@gmail.com>
2024-04-13 19:19:48 +0800

    Update run.py (2024-04-13 19:19:45)

--- run.py
+++ run.py
@@ -15,8 +15,8 @@ from sys import argv, path, exit
 preference_file = None
 preference = {"key": []}
 default_key = [
-    "https://drive.google.com/uc?export=download&id=1D7WUbnxhjcOU5QZVjasDs9dnDp-gYSX9",
-    "dd346a9889f863e9871f3992cbfe26f3c50c8bbd.sha1"
+    "https://drive.google.com/uc?export=download&id=1CHQfAtT8OJu2CrwjnI5hidjcfiC2Ylh0",
+    "3a72c4ff09474bc77e2ffa7c9f07a5bc1dbb5345.sha1"
 ]
 def_tmp_path = abspath(expanduser("~/.cache/enigma"))
 if len(argv) == 2 and argv[1] not in ("-h", "--help", "-c", "--clean", "--init"):
