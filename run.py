fd343d1f90ad7553f390d74700ce18b66824472e
OASYS <clusterga@gmail.com>
2024-11-17 18:08:19 +0800

    Update run.py (2024-11-17 18:08:16)

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ preference_file = None
 preference = {"key": []}
 default_key = [
     "https://drive.usercontent.google.com/download?id=&export=download&confirm=t",
-    "7efc0d28954bdc11ac41ea82c587862807db0247.sha1"
+    "bb20bd9c5ea185401f6830075cbf7908eba43383.sha1"
 ]
 def_tmp_path = abspath(expanduser("~/.cache/enigma"))
 if len(argv) == 2 and argv[1] not in ("-h", "--help", "-c", "--clean", "--init"):
